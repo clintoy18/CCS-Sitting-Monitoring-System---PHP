e32bf5c56c9ca02db5af2b503dd96a4f490a25ef
clintoy18 <116106899+clintoy18@users.noreply.github.com>
2025-02-11 17:16:26 +0800

    adjusted paddings and margins

--- dashboard.php
+++ dashboard.php
@@ -9,9 +9,9 @@ include "auth.php";
 
 <div class="px-8 py-8 ps-8 grid grid-cols-3 gap-4">
    
-    <div class="box-content bg-blue-900 md:box-border border-4 p-4 text-white justify-items-center "> <h1 class="text-center text-xl">Student Information</h1>
-    <img src="ccslogo.png" alt="" class="rounded-full size-32">
-    <div class="flex-items-start">
+    <div class="box-content bg-blue-900 md:box-border border-4 p-8 text-white "> <h1 class="text-center text-xl pb-4">Student Information</h1>
+    <img src="ccslogo.png" alt="" class="rounded-full mx-auto ">
+    <div class="flex-items-start p-8">
     <p><b>IDNO: </b><?php echo $userID ?></p>
     <p><b>Name: </b><?php echo $ufname . "  ".$ulname  ?></p>
     <p><b>Course:</b>  <?php echo $course ?> </p>
@@ -21,11 +21,23 @@ include "auth.php";
     <p><b>Session:</b> </p>
     <p></p>
     </div>
+    </div>
+    <div class="box-content bg-blue-900 md:box-border border-4 p-8 text-white"><h1 class="text-center text-xl pb-4 ">Announcement</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sit libero odit eveniet, ad,
+         velit necessitatibus temporibus placeat nisi, ducimus praesentium unde nemo est neque delectus quidem! 
+         Quisquam, maiores tempore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugiat ut amet ea quod exercitationem at quaerat repellendus nemo odio blanditiis,
+         adipisci rerum! Explicabo excepturi natus corrupti optio enim modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia culpa excepturi laborum nesciunt eaque 
+         laboriosam ratione molestias architecto consectetur! Nihil natus non hic quibusdam at cum est similique id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos maxime hic corporis, quaerat delectus rem autem est quam accusamus, similique nulla recusandae, voluptas quos aliquam dolores modi nihil repellat! Perferendis?
+    </p>
+    </div>
 
-    </div>
-    <div class="box-content bg-blue-900 md:box-border border-4 p-4 text-white"><h1 class="text-center text-xl">Announcement</h1>
-    </div>
-    <div class="box-content bg-blue-900 md:box-border border-4 p-4 text-white"><h1 class="text-center text-xl">Rules and Regulation</h1>
+    <div class="box-content bg-blue-900 md:box-border border-4 p-8 text-white"><h1 class="text-center text-xl pb-4">Rules and Regulations</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sit libero odit eveniet, ad,
+         velit necessitatibus temporibus placeat nisi, ducimus praesentium unde nemo est neque delectus quidem! 
+         Quisquam, maiores tempore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugiat ut amet ea quod exercitationem at quaerat repellendus nemo odio blanditiis,
+         adipisci rerum! Explicabo excepturi natus corrupti optio enim modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia culpa excepturi laborum nesciunt eaque 
+         laboriosam ratione molestias architecto consectetur! Nihil natus non hic quibusdam at cum est similique id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos maxime hic corporis, quaerat delectus rem autem est quam accusamus, similique nulla recusandae, voluptas quos aliquam dolores modi nihil repellat! Perferendis?
+    </p>
     </div>
 
 
