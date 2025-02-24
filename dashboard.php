eb6d29ce705ea9bd0cd82a9f006a5044bb6c4470
clintoy18 <aicsalonzo@gmail.com>
2025-02-24 18:53:57 +0800

    responsive

--- dashboard.php
+++ dashboard.php
@@ -4,45 +4,62 @@ include "layout.php";
 include "auth.php";
 ?>
 
-<div class="px-8 py-8 ps-8 grid grid-cols-3 gap-2 ">
-    <div class="rounded-lg border bg-white border-gray-300 p-6 hover:bg-blue-500 hover:rounded-xl transition-all duration-300">
-        <h1 class="text-center text-xl pb-4">Student Information</h1>
+<div class="px-4 py-8 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 items-stretch animate-fade-in">
 
-        <?php if (!empty($userData['profile_picture'])): ?>
-            <img src="<?php echo $userData['profile_picture']; ?>"
-                alt="Profile Picture"
-                class="w-64 h-64 rounded-full border-4 border-gray-300 object-cover aspect-square justify-self-center mb-6">
-        <?php endif; ?>
+    <!-- Student Information Card -->
+    <div class="rounded-lg border bg-white border-gray-300 p-6 shadow-lg 
+                transition-all duration-300 hover:shadow-2xl hover:-translate-y-2 flex flex-col h-full animate-slide-up">
+        <h1 class="text-center text-2xl font-extrabold pb-4 text-gray-800 dark:text-white">Student Information</h1>
 
-        <div class="flex flex-col space-y-4 ">
-            <p class="flex items-center"><b class="w-16">IDNO</b><?php echo ": " . $userData['idno'] ?></p>
-            <p class="flex items-center"><b class="w-16">Name</b><?php echo ": " . $userData['fname'] . " " . $userData['lname'] ?></p>
-            <p class="flex items-center"><b class="w-16">Course</b><?php echo ": " . $userData['course'] ?></p>
-            <p class="flex items-center"><b class="w-16">Year</b><?php echo ": " . $userData['year_level'] ?></p>
-            <p class="flex items-center"><b class="w-16">Address</b><?php echo ": " . $userData['address'] ?></p>
-            <p class="flex items-center"><b class="w-16">Session</b><?php echo ": " . $userData['session'] ?></p>
+        <div class="flex justify-center mb-6">
+            <?php if (!empty($userData['profile_picture'])): ?>
+                <img src="<?php echo $userData['profile_picture']; ?>" 
+                    alt="Profile Picture" 
+                    class="w-40 h-40 rounded-full border-4 border-blue-500 object-cover transition-all duration-300 hover:scale-110 hover:ring-4 hover:ring-blue-400 aspect-square shadow-md">
+            <?php else: ?>
+                <div class="w-40 h-40 flex items-center justify-center rounded-full border-4 border-gray-300 bg-gray-100 aspect-square shadow-md">
+                    <span class="text-gray-500">No Image</span>
+                </div>
+            <?php endif; ?>
+        </div>
+
+
+        <div class="space-y-4 text-lg font-semibold text-gray-800 dark:text-gray-300 flex-1 text-justify">
+            <p><b class="text-blue-600 dark:text-blue-400">ID No:</b> <?php echo $userData['idno']; ?></p>
+            <p><b class="text-blue-600 dark:text-blue-400">Name:</b> <?php echo $userData['fname'] . " " . $userData['lname']; ?></p>
+            <p><b class="text-blue-600 dark:text-blue-400">Course:</b> <?php echo $userData['course']; ?></p>
+            <p><b class="text-blue-600 dark:text-blue-400">Year:</b> <?php echo $userData['year_level']; ?></p>
+            <p><b class="text-blue-600 dark:text-blue-400">Address:</b> <?php echo $userData['address']; ?></p>
+            <p><b class="text-blue-600 dark:text-blue-400">Session:</b> <?php echo $userData['session']; ?></p>
         </div>
     </div>
 
-    <div class="rounded-lg border bg-white border-gray-300 p-6 hover:bg-blue-500 hover:rounded-xl transition-all duration-300">
-        <h1 class="text-center text-xl pb-4 ">Announcement</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sit libero odit eveniet, ad,
-            velit necessitatibus temporibus placeat nisi, ducimus praesentium unde nemo est neque delectus quidem!
-            Quisquam, maiores tempore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugiat ut amet ea quod exercitationem at quaerat repellendus nemo odio blanditiis,
-            adipisci rerum! Explicabo excepturi natus corrupti optio enim modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia culpa excepturi laborum nesciunt eaque
-            laboriosam ratione molestias architecto consectetur! Nihil natus non hic quibusdam at cum est similique id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos maxime hic corporis, quaerat delectus rem autem est quam accusamus, similique nulla recusandae, voluptas quos aliquam dolores modi nihil repellat! Perferendis?
+    <!-- Announcements Card -->
+    <div class="rounded-lg border bg-white border-gray-300 p-6 shadow-lg 
+                transition-all duration-300 hover:shadow-2xl hover:-translate-y-2 flex flex-col h-full animate-slide-up [animation-delay:0.3s]">
+        <h1 class="text-center text-2xl font-extrabold pb-4 text-gray-800 dark:text-white">Announcements</h1>
+        <p class="text-lg font-medium text-gray-700 dark:text-gray-300 flex-1 leading-relaxed text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed justo in lacus accumsan molestie. <br><br>
+            Suspendisse potenti. Curabitur auctor, turpis ut vehicula eleifend, risus nisl scelerisque arcu, 
+            in pharetra risus libero in est. Aliquam erat volutpat. Donec scelerisque lectus quis sapien egestas, 
+            a maximus augue tempor.
+            Nulla facilisi. Aenean eget justo vel leo egestas bibendum a et elit. Vivamus eget libero et neque 
+            dictum feugiat eget at lacus. 
+            Proin rhoncus, velit sit amet vehicula tincidunt, orci velit tincidunt lectus, a tempus turpis erat 
+            at libero. Suspendisse quis leo at dolor sollicitudin efficitur.
         </p>
     </div>
 
-    <div class="rounded-lg border bg-white border-gray-300 p-6 hover:bg-blue-500 hover:rounded-xl transition-all duration-300">
-        <h1 class="text-center text-xl pb-4">Rules and Regulations</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sit libero odit eveniet, ad,
-            velit necessitatibus temporibus placeat nisi, ducimus praesentium unde nemo est neque delectus quidem!
-            Quisquam, maiores tempore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugiat ut amet ea quod exercitationem at quaerat repellendus nemo odio blanditiis,
-            adipisci rerum! Explicabo excepturi natus corrupti optio enim modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia culpa excepturi laborum nesciunt eaque
-            laboriosam ratione molestias architecto consectetur! Nihil natus non hic quibusdam at cum est similique id? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos maxime hic corporis, quaerat delectus rem autem est quam accusamus, similique nulla recusandae, voluptas quos aliquam dolores modi nihil repellat! Perferendis?
+    <!-- Rules & Regulations Card -->
+    <div class="rounded-lg border bg-white border-gray-300 p-6 shadow-lg 
+                transition-all duration-300 hover:shadow-2xl hover:-translate-y-2 flex flex-col h-full animate-slide-up [animation-delay:0.6s]">
+        <h1 class="text-center text-2xl font-extrabold pb-4 text-gray-800 dark:text-white">Rules & Regulations</h1>
+        <p class="text-lg font-medium text-gray-700 dark:text-gray-300 flex-1 leading-relaxed text-justify">
+            Nulla facilisi. Aenean eget justo vel leo egestas bibendum a et elit. Vivamus eget libero et neque 
+            dictum feugiat eget at lacus. 
+            Proin rhoncus, velit sit amet vehicula tincidunt, orci velit tincidunt lectus, a tempus turpis erat 
+            at libero. Suspendisse quis leo at dolor sollicitudin efficitur.
         </p>
     </div>
-
 
 </div>
